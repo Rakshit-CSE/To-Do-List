7c43ade06c5c6309bebb7a8ea497f1947db52f6a
Rakshit <98008663+Rakshit-CS-AIML@users.noreply.github.com>
2023-06-21 15:54:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// Creating a new Todo item
+$('#inp').keypress(function(e)
+{
+    //console.log(e.which);
+    if(e.which===13)
+    {
+        const todoText = $('#inp').val();
+        $('#list').append(`<li><span>X</span> ${todoText}</li>`);
+        $('#inp').val("");
+    }
+});
+
+// Mark the todo as completed
+$('ul').on('click','li',function(){
+    $(this).toggleClass('completed');
+});
+
+//Remove list item
+$('#list').on('click','span',function(e){
+    $(this).parent().fadeOut(700,function(){
+        $(this).remove();
+    });
+    e.stopPropagation();x
+});
+
+//Adding function to plus sign
+$('#plus').click(function () {
+    $('#inp').fadeToggle();
+})
